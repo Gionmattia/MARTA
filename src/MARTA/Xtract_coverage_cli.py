f0477995c410017724a57b30fe4487c434a66fa4
gionmattia <gionmattia@gmail.com>
2026-04-13 14:59:01 +0100

    Added messages during execution!

--- src/MARTA/Xtract_coverage_cli.py
+++ src/MARTA/Xtract_coverage_cli.py
@@ -9,6 +9,7 @@ extract-coverage --bigwigs <path> --gtf <path> --txid ENSMUST00000180036 --outpu
 
 # ~~~ Libraries ~~~
 import argparse
+from pathlib import Path
 from MARTA.Xtract_coverage import *
 
 # ~~~ Inputs Parsing ~~~
@@ -47,7 +48,9 @@ def parse_args():
 def main():
     args = parse_args()
 
+    print(f"[START] Welcome!")
     # 0 List all bigwigs in the given directory
+    print(f"[1/5] Looking for bigwig files...")
     folder_path = Path(args.bigwigs)
     if not folder_path.exists():
         raise FileNotFoundError(f"Bigwig folder not found: {folder_path}")
@@ -57,21 +60,25 @@ def main():
     bigwig_files = [str(f.resolve()) for f in folder_path.glob("*.bw")]
     if not bigwig_files:
         raise FileNotFoundError(f"No bigwigs files found in: {folder_path}")
+    print(f"[1/5] Found {len(bigwig_files)} bigwig files...")
 
+    print(f"[2/5] Loading annotation file...")
     # 1 Retrieve transcript ranges
     gr = read_gtf_annotation(args.gtf)
+    print(f"[3/5] Retrieving coordinates for {args.txid}...")
     transcript_ranges = extract_tx_pyranges(gr, args.txid)
 
     # 2  Retrieve transcript features (CDS, UTRs) ranges and strandedness
     all_ranges, strandedness = regions_df_formatter(transcript_ranges)
 
+    print(f"[4/5] Extracting coverage for {args.txid}...")
     # 3 Extract coverage from given bigwigs
     all_runs_df = extract_from_multiple_bigwigs(bigwig_files, all_ranges, strandedness, n_jobs=1)
 
     # 4 Remove genomic coordinate and save file
     all_runs_df = all_runs_df.drop(columns="Position")
 
-
+    print(f"[5/5] Saving coverage table at {args.output}")
     # 5 Assemble file name and output path
     out_name = args.txid + "_raw_counts.csv"
     out_dir = Path(args.output)
@@ -79,6 +86,8 @@ def main():
     outpath = out_dir / out_name
 
     all_runs_df.to_csv(outpath, index=False)
+
+    print(f"[FINISH] Run Complete! :)")
 
 
 
